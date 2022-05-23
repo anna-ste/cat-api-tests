e59ada71326b482522ba77cae8311eaa21c7bc28
anna-ste <anna.ste0502@gmail.com>
2022-05-23 02:30:02 +0200

    Add test validate categories list json schema

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -18,3 +18,12 @@ def test_category_not_in_the_list():
     assert response.status_code == 200, 'Incorrect status code'
     categories_names = response_handler.get_list_category_names(response)
     assert CD.NON_EXISTING_CATEGORY not in categories_names
+
+
+def test_category_list_json_schema():
+    schema = schema_loader.get_json_schema(schema="category_list")
+    response = request.get_categories()
+
+    # check response corresponds to schema
+    assert response_handler.validate_json(response, schema), 'Category list schema is not valid'
+
